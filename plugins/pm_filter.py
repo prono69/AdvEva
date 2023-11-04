35bbc217be1bca031f233a98cff3123d487d6b11
faltu9851@gmail.com <faltu9851@gmail.com>
2023-11-04 12:35:12 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -459,7 +459,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
          buttons = [[
             InlineKeyboardButton('🤖 Updates', url='https://t.me/Neko_Drive'),
-            InlineKeyboardButton('♥️ Source', callback_data='source')
+            InlineKeyboardButton('♥️ Source Code', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
